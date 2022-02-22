c428e97b5166082c636dd456c358b3c954ba41ee
jihaneh <jijih@hotmail.fr>
2022-02-22 10:35:56 +0100

    Update Books using Book component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import booksList from '../datas/BooksList';
+import Book from './Book';
 
 export default function Books() {
-  return <div>Books</div>;
+  return (
+    <div className="books-container">
+      {booksList.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </div>
+  );
 }
