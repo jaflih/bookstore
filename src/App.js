1074b2ec550710ae1c4b4910410b5199b490bd42
jihaneh <jijih@hotmail.fr>
2022-02-22 08:49:22 +0100

    Add 2 routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App" />;
-}
+import { Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  );
+}
