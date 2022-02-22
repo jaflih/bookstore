b9621aaec808bccfec310e3bb26dbc196c6bd3b4
jihaneh <jijih@hotmail.fr>
2022-02-22 16:18:07 +0100

    Add the react-redux provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import BooksContainer from './pages/BooksContainer';
 import Categories from './components/Categories';
 import Header from './components/Header';
+import store from './redux/configureStore';
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<BooksContainer />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<BooksContainer />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
